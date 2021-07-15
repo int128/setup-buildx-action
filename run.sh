4b7170c41fb8424fa2dcea79643f7f8a92bc2206
Hidetake Iwata <int128@gmail.com>
2021-07-15 18:08:46 +0900

    Add driver-opts option (#2)

--- run.sh
+++ run.sh
@@ -7,6 +7,13 @@ binary_url="https://github.com/docker/buildx/releases/download/${BUILDX_VERSION}
 mkdir -vp ~/.docker/cli-plugins
 curl -sfSL -o ~/.docker/cli-plugins/docker-buildx "$binary_url"
 chmod +x ~/.docker/cli-plugins/docker-buildx
-docker buildx create --use
+
+# https://github.com/docker/buildx/blob/master/docs/reference/buildx_create.md
+buildx_args=('--use')
+if [[ $BUILDX_DRIVER_OPTS ]]; then
+  buildx_args+=('--driver-opt' "$BUILDX_DRIVER_OPTS")
+fi
+docker buildx create "${buildx_args[@]}"
+
 docker buildx version
 docker buildx inspect
